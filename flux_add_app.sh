83d396d57e21106e8a7ad8763ac61c87b26a603b
Irene Jaramillo <ijaramil@starbucks.com>
2025-05-08 14:12:35 -0700

    Created a flux source yaml that points to flux_demo wordpress_app branch

--- flux_add_app.sh
+++ flux_add_app.sh
@@ -13,7 +13,33 @@ flux create source git wordpressapp \
   --export > ./clusters/my-cluster/wordpressapp-source.yaml
 
 # Commit and push the wordpressapp-source.yaml file to the flux-gs repository:
-git add -A && git commit -m "Add flux_demo GitRepository branch wordpress_app as source"
+git add -A && git commit -m "Created a flux source yaml that points to flux_demo wordpress_app branch"
 git push
 
 kubectl get all
+kubectl get namespaces
+
+# Deploy application: 
+# flux create command creates a Kustomization file that applies the wordpressapp deployment
+# create tells Flux to build and apply the kustomize file located in the flux_demo repo
+echo "Creating kustomization and deploying wordpressapp app..."
+
+flux create kustomization wordpressapp \
+  --target-namespace=default \
+  --source=wordpressapp \
+  --path="./wordpressapp/kustomize" \
+  --prune=true \
+  --wait=true \
+  --interval=30m \
+  --retry-interval=2m \
+  --health-check-timeout=3m \
+  --export > ./clusters/my-cluster/podinfo-kustomization.yaml
+
+# Commit and push the podinfo-kustomization.yaml file to the flux-gs repository:
+git add -A && git commit -m "Add podinfo Kustomization"
+git push
+
+flux get kustomizations --watch &
+
+echo "Waiting for podinfo app to be deployed..."
+kubectl -n default get deployments,services

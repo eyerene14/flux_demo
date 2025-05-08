5ce72b320ef6cacddb3c81f243e82486c5ed3c30
Irene Jaramillo <ijaramil@starbucks.com>
2025-05-08 09:48:42 -0700

    Created: initial Getting Started with Flux script

---
+++ flux_get_started.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#source: https://fluxcd.io/flux/get-started/
+
+#Install flux: brew install fluxcd/tap/flux
+
+export GITHUB_TOKEN=$(cat ~/.pat/eyerene14_github_pat)
+export GITHUB_USER=eyerene14
+
+# Install Flux onto your cluster
+# flux bootstrap command: 
+# - Creates repo <fleet-infra> in your github account
+# - Adds Flux component manifests to the repository
+# - Deploys Flux Components to your Kubernetes Cluster.
+# - Configures Flux components to track the path /clusters/my-cluster/ in the repository
+
+echo "Bootstrapping flux to the cluster..."
+
+flux bootstrap github \
+  --owner=$GITHUB_USER \
+  --repository=flux-gs \
+  --branch=main \
+  --path=./clusters/my-cluster \
+  --personal
+
+# Clone the flux-gs repository to your local machine:
+echo "Cloning the flux-gs repository and changing directory..."
+cd ..
+git clone https://github.com/$GITHUB_USER/flux-gs
+cd flux-gs
+
+# Create a GitRepository manifest pointing to podinfo repository’s master branch:
+echo "Creating GitRepository manifest for podinfo..."
+# flux create command creates a GitRepository file that points to the podinfo repo
+flux create source git mypodinfo \
+  --url=https://github.com/stefanprodan/podinfo \
+  --branch=master \
+  --interval=1m \
+  --export > ./clusters/my-cluster/podinfo-source.yaml
+
+# Commit and push the podinfo-source.yaml file to the flux-gs repository:
+git add -A && git commit -m "Add podinfo GitRepository"
+git push
+
+# Deploy application: 
+# flux create command creates a Kustomization file that applies the podinfo deployment
+# create tells Flux to build and apply the kustomize file located in the podinfo repo
+echo "Creating kustomization and deploying podinfo app..."
+
+flux create kustomization mypodinfo \
+  --target-namespace=default \
+  --source=podinfo \
+  --path="./kustomize" \
+  --prune=true \
+  --wait=true \
+  --interval=30m \
+  --retry-interval=2m \
+  --health-check-timeout=3m \
+  --export > ./clusters/my-cluster/podinfo-kustomization.yaml
+
+# Commit and push the podinfo-kustomization.yaml file to the flux-gs repository:
+git add -A && git commit -m "Add podinfo Kustomization"
+git push
+
+flux get kustomizations --watch &
+
+echo "Waiting for podinfo app to be deployed..."
+kubectl -n default get deployments,services
+
+
